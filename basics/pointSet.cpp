463d9dbae481c0fc73068594bdc7aaa2dd4d3f46
Elias CROZE <elias.croze@gmail.com>
2014-08-31 16:05:48 +0800

    Typo fixed in pointSet.cpp

--- basics/pointSet.cpp
+++ basics/pointSet.cpp
@@ -1,6 +1,5 @@
 #include "pointSet.h"
 #include "lmath.h"
-#include <iostream>
 
 int PointSet::addPoint(LongInt x1, LongInt y1){
 	struct MyPoint thisPoint;
@@ -21,9 +20,9 @@ int PointSet::inCircle(int p1Idx, int p2Idx, int p3Idx, int pIdx) {
 	int det1 = signDet(a.x, a.y, 1,
 		b.x, b.y, 1,
 		c.x, c.y,1);
-	int det2 = signDet(a.x - p.x, a.y - p.y, (a.x*a.x - p.x*p.x)+(a.y*a.y - p.y*p.y),
-		b.x - p.x, b.y - p.y, (b.x*b.x - p.x*p.x)+(b.y*b.y - p.y*p.y),
-		c.x - p.x, c.y - p.y, (c.x*c.x - p.x*p.x)+(c.y*c.y - p.y*p.y));
+	int det2 = signDet(a.x - p.x, a.y - p.y, square((a.x - p.x))+square(a.y - p.y),
+		b.x - p.x, b.y - p.y, square(b.x - p.x)+square(b.y - p.y),
+		c.x - p.x, c.y - p.y, square(c.x - p.x)+square(c.y - p.y));
 	return det1*det2;
 }
 
